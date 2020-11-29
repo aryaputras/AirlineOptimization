385ad58ce690cf24cd387c393eae5e193ecbfbda
aryaputras <abilabil147@gmail.com>
2020-11-30 06:35:50 +0700

    Initial Commit

---
+++ AirlineOptimization/AirlineOptimizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirlineOptimizationApp.swift
+//  AirlineOptimization
+//
+//  Created by Abigail Aryaputra Sudarman on 30/11/20.
+//
+
+import SwiftUI
+
+@main
+struct AirlineOptimizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirlineOptimization/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AirlineOptimization
+//
+//  Created by Abigail Aryaputra Sudarman on 30/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
